4844b0177e29b9976d5ef5aac6fedadbe0e1f4a8
Cristina Rivas <a18CristinaRP@iessanclemente.net>
2019-05-20 18:58:34 +0200

    nuevo bucle, empezando elemento medicamento

--- parseandoVademecum.py
+++ parseandoVademecum.py
@@ -15,9 +15,8 @@ pagePA = requests.get('https://www.vademecum.es/principios-activos-a_1')
 treePA = html.fromstring(pagePA.content)
 principiosActivos = tree.xpath('//div[3]/div/ul/li/a')
 
-
 #creo elemento medicamento
-for i in range (0, numeroRegistros+1:
+for i in range (0, numeroRegistros+1):
     NombreMedicamento = nombresMedicamentos[i].xpath('text()')[0]
     codigoMedicamento = random.randint (0,999999999)
     principioActMedicamento = principiosActivos[i].xpath('text()')[0]
