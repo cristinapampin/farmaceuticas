d09c29061b9de35faee52cff6df84f406a88b572
dawMP - Rivas Pampín <a18cristinarp@a50eqd00.sanclemente.local>
2019-05-20 17:48:44 +0200

    scrapeando vademecum. medicamentos check

---
+++ parseandoVademecum.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from lxml import html
+import requests
+import xml.etree.ElementTree as ET
+
+page = requests.get('https://www.vademecum.es/medicamentos-a_1')
+tree = html.fromstring(page.content)
+
+
+nombresMedicamentos = tree.xpath('//div/section/div[3]/div/ul/li/a')
+numeroMedicamentos = len(nombresMedicamentos)
+
+for i in range (numeroMedicamentos):
+    medicamento = nombresMedicamentos[i].xpath('text()')
+    print (medicamento)
